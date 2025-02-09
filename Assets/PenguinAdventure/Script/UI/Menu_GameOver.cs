7281ac99a6d1ec0fd06f3c23cccf6b1202b51169
AtulAbhiwan03 <139665307+TanuTechnologies@users.noreply.github.com>
2025-02-09 18:40:54 +0530

    base

---
+++ Assets/PenguinAdventure/Script/UI/Menu_GameOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_GameOver : MonoBehaviour {
+	public GameObject Next;
+	// Use this for initialization
+	void Start () {
+		if (GlobalValue.levelPlaying >= GameManager.HighestLevel || GameManager.instance.isFinishWorld)
+			Next.SetActive (false);
+		Advertisements.Instance.ShowInterstitial();
+
+	}
+}
